f9bce8b7adce305c8f187f18f4bcb27aecfaf7e1
jpbrutos22 <jp22082005@gmail.com>
2026-02-10 14:36:26 -0300

    Aula 1 POO

---
+++ Aula 1 POO/src/Main.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class Main {
+
+    static void main(String[] args) {
+
+        int x = 10;
+        int y = 20;
+
+        System.out.println("O valor é: " + (x+y));
+
+        System.out.println("Hello World!");
+
+    }
+}
